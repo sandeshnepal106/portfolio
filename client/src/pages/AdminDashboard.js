fa361f7ace88d1927853c959f7769819b91f2059
Sandesh Nepal <sandeshnepal106@gmail.com>
2026-03-23 22:00:50 +0530

    Fixed the deployment problem

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -33,6 +33,7 @@ export default function AdminDashboard() {
     useEffect(() => {
         // Authenticate check essentially happens via the contacts fetch due to adminAuth middleware
         fetchContacts();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const handleLogout = async () => {
